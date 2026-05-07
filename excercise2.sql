5e24d7ad9d0590c35fcf6a6df095fcbf0d10b690
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-05-07 14:15:15 +0700

    feat : price above average per customer

---
+++ excercise2.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS transaction(
+    id SERIAL PRIMARY KEY,
+    customer_id int,
+    amount INT
+);
+
+TRUNCATE TABLE transaction
+
+INSERT INTO transaction(customer_id, amount)
+VALUES
+('101', 500),
+('101', 300),
+('102', 150);
+
+
+select t.customer_id,t.amount, (select avg(t2.amount) from transaction t2
+where t.customer_id = t2.customer_id ) as average  from transaction t
+where t.amount  >= (select avg(t2.amount) from transaction t2
+where t.customer_id = t2.customer_id ) ;
+
+
+
+
+
+-- select t.customer_id,t.amount  from transaction t
+-- where (select avg(t2.amount) from transaction t2);
